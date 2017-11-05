62370db3d7dfac20742ca8d19c8a0607fa56c8bb
rykun <rykunek@hotmail.com>
2017-11-05 01:22:22 +0100

    login, logout redirections

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+
+  renderContent() {
+    switch(this.props.auth) {
+      case null:
+        return
+      case false:
+        return <li><a href="/auth/google">Login With Google</a></li>;
+      default:
+        return <li><a href="/api/logout">Logout</a></li>;
+    }
+  }
+
   render() {
     return (
         <nav>
           <div className="nav-wrapper">
-            <a href="#" className="brand-logo left">Feedback App</a>
+            <Link 
+              to={this.props.auth ? '/surveys' : '/'}
+              className="brand-logo left"
+            >
+              Feedback App
+            </Link>
             <ul id="nav-mobile" className="right">
-              <li>
-                <a>Login With Google</a>
-              </li>
+              {this.renderContent()}
             </ul>
           </div>
         </nav>
@@ -17,4 +34,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps, null)(Header);
