dc3fa4a1f30227d2248f37d8d1b3ca759f9af1f8
rykun <rykunek@hotmail.com>
2017-11-05 14:06:53 +0100

    add stripe billing system and auth middleware

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
 
 require('./models/User');
 require('./services/passport');
@@ -12,6 +13,7 @@ mongoose.connect(keys.mongoURI, {useMongoClient: true});
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -22,7 +24,9 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT,()=>{
