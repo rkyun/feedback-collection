be1d74e1830d04cc71e5c984deaf98517ec8e1f0
rykun <rykunek@hotmail.com>
2017-11-05 01:15:53 +0100

    Landing component

---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h1>Feedback app</h1>
+      Collect feedback from your users!
+    </div>
+  )
+}
+
+
+export default Landing;
